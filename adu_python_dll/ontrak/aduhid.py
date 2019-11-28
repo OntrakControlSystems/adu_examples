b480be88312ce98e8d2cbfdc3bfd5b5d45d95575
James Acres <james@jamesacres.com>
2019-11-28 10:20:00 -0500

    updated AduHid python example

--- adu_python_dll/ontrak/aduhid.py
+++ adu_python_dll/ontrak/aduhid.py
@@ -1,7 +1,11 @@
+# Wrapper for OnTrak Control Systems' AduHid.dll (USB/Device Pipe Functions)
+# See: https://www.ontrak.net/ADUSDK/Functions.html
+
 from ctypes import *
 from ctypes.wintypes import *
 import platform
 import os
+
 ontrak_vendor_id = 0x0A07
 INVALID_HANDLE_VALUE = c_void_p(-1).value
 
@@ -57,9 +61,14 @@ _read_adu_device.argtypes = [POINTER(HANDLE), c_char_p, c_ulong, POINTER(c_ulong
 
 adu_handle_type = POINTER(HANDLE)
 
+# Get the number of connected ADU devices
+# Returns number of connected devices
 def count(timeout):
 	return _adu_count(timeout)
 
+# Get a list connected ADU devices. Allows access to vendor_id, product_id, and serial_number of each device
+# See documentation link at top of page for explanation of timeout 
+# Returns a list of ADU_DEVICE_ID structures
 def device_list(timeout):
 	num_adus = _adu_count(timeout)
 	device_list = (ADU_DEVICE_ID * num_adus)()
@@ -68,27 +77,62 @@ def device_list(timeout):
 	_get_adu_device_list(device_list, num_adus, timeout, byref(num_devices_found), byref(result))
 	return device_list
 
+# Display a Windows GUI popup with a list of ADU devices, allowing selection of one.
+# Returns the selected ADU_DEVICE_ID object which that then be used to open the device via a serial number or product id.
+# Returns None if no device was selecte from list
 def show_device_list(header_string):
 	device_id = ADU_DEVICE_ID()
 	_show_adu_device_list(device_id, c_char_p(header_string))
+
+	if device_id.product_id == 0x00 and device_id.vendor_id == 0x00:
+		return None
+
 	return device_id
 
-def open_device_by_product_id(product_id, timeout):
-	device_handle = _open_adu_device_by_product_id(product_id, timeout)
+# Searches for any ADU device attached to the computer. See documentation link at top of page for explanation of timeout 
+# The function opens the first ADU device that it encounters and passes back a handle to it. 
+# Returns a device handle on success, and None on failure
+def open_adu_device(timeout):
+	device_handle = _open_adu_device(timeout)
+
+	if device_handle == 0 or device_handle == INVALID_HANDLE_VALUE:
+		return None
+
 	return device_handle
 
+# Open an ADU device by product ID. See documentation link at top of page for explanation of timeout 
+# Returns a device handle on success, and None on failure
+def open_device_by_product_id(product_id, timeout):
+	device_handle = _open_adu_device_by_product_id(product_id, timeout)
+
+	if device_handle == 0 or device_handle == INVALID_HANDLE_VALUE:
+		return None
+
+	return device_handle
+
+# Open an ADU device by serial number. See documentation link at top of page for explanation of timeout 
+# Returns a device handle on success, and None on failure
 def open_device_by_serial_number(serial_number, timeout):
 	device_handle = _open_adu_device_by_serial_number(serial_number, timeout)
+
+	if device_handle == 0 or device_handle == INVALID_HANDLE_VALUE:
+		return None
+
 	return device_handle
 	
+# Close a connected ADU device
 def close_device(device_handle):
 	_close_adu_device(device_handle)
 
+# Write a command to the device. The command is an ASCII string representing the command, such as 'SK0'
+# Returns 0 if failed, other value if successful
 def write_device(device_handle, command, timeout):
 	bytes_written = c_ulong()
 	result = _write_adu_device(device_handle, c_char_p(command), len(command), byref(bytes_written), timeout)
 	return result
 	
+# Read a pending value from the device. Data will be pending if a responsive command was previously issued (such as 'RPK0')
+# Returns the result of the read (0 if successul, other value if failure). Value will contain the read value on success, and None on failure
 def read_device(device_handle, timeout):
 	read_string = '#' * 7
 	read_buffer = c_char_p(read_string)
@@ -99,4 +143,3 @@ def read_device(device_handle, timeout):
 		return (result, None)
 
 	return (result, int(read_string.rstrip('\x00')))
-	
